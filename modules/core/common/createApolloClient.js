316af423b25b62ae934e173786a9c640d654056d
Philip Balo <f.g.balo@gmail.com>
2019-03-13 15:46:25 +0200

    Added handler and inspect errors in GraphQL network stack.

--- modules/core/common/createApolloClient.js
+++ modules/core/common/createApolloClient.js
@@ -3,6 +3,7 @@ import fetch from 'isomorphic-unfetch';
 /* eslint-eneble */
 
 import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
 import { LoggingLink } from 'apollo-logger';
 import { withClientState } from 'apollo-link-state';
 import AWSAppSyncClient, { AUTH_TYPE, createAppSyncLink, createLinkWithCache } from 'aws-appsync';
@@ -11,6 +12,14 @@ import aws_config from '../../../packages/client/src/aws-exports';
 import log from './log';
 
 const createApolloClient = resolvers => {
+  const onErrorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.map(({ message, locations, path }) =>
+        console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
+
   const stateLink = createLinkWithCache(cache =>
     withClientState({
       cache,
@@ -29,7 +38,7 @@ const createApolloClient = resolvers => {
 
   const logger = new LoggingLink({ logger: log.debug.bind(log) });
 
-  const link = ApolloLink.from([logger, stateLink, appSyncLink]);
+  const link = ApolloLink.from([logger, onErrorLink, stateLink, appSyncLink]);
 
   return new AWSAppSyncClient({ disableOffline: true }, { link });
 };
